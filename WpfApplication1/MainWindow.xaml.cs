0f8d3140fdccd7f21b2fa9d21d92bbf5a16ea302
Israel <Israel@github.com>
2017-11-25 22:49:26 +0200

    New message

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace WpfApplication1
         public MainWindow()
         {
             InitializeComponent();
+            MessageBox.Show("Hi", "Hi", MessageBoxButton.OKCancel, MessageBoxImage.Hand);
         }
     }
 }
